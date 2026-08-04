155bc0b690b77420e06a2422fab09b76e4804632
Manel Ben Salah <manelbensalah48@gmail.com>
2026-08-04 18:50:31 +0200

    feat: validate deposit amount

--- src/main/java/com/sg/kata/bankaccount/model/Account.java
+++ src/main/java/com/sg/kata/bankaccount/model/Account.java
@@ -7,10 +7,21 @@ public class Account {
     private BigDecimal balance = BigDecimal.ZERO;
 
     public void deposit(BigDecimal amount) {
+
+        validateAmount(amount);
+
         balance = balance.add(amount);
     }
 
     public BigDecimal getBalance() {
         return balance;
+    }
+
+    private void validateAmount(BigDecimal amount) {
+
+        if (amount == null || amount.signum() <= 0) {
+            throw new IllegalArgumentException(
+                    "Amount must be strictly positive");
+        }
     }
 }
