b728d0b85b5045f49f4d2db8b01b76f44418fe7b
Manel Ben Salah <manelbensalah48@gmail.com>
2026-08-04 18:44:29 +0200

    test: define invalid deposit behavior

--- src/test/java/com/sg/kata/bankaccount/model/AccountTest.java
+++ src/test/java/com/sg/kata/bankaccount/model/AccountTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class AccountTest {
 
     @Test
@@ -19,5 +19,37 @@ class AccountTest {
         // Then
         assertThat(account.getBalance())
                 .isEqualByComparingTo(new BigDecimal("100.00"));
+    }
+    @Test
+    void should_reject_zero_deposit() {
+
+        Account account = new Account();
+
+        assertThatThrownBy(() ->
+                account.deposit(BigDecimal.ZERO))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Amount must be strictly positive");
+    }
+
+    @Test
+    void should_reject_negative_deposit() {
+
+        Account account = new Account();
+
+        assertThatThrownBy(() ->
+                account.deposit(new BigDecimal("-10.00")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Amount must be strictly positive");
+    }
+
+    @Test
+    void should_reject_null_deposit() {
+
+        Account account = new Account();
+
+        assertThatThrownBy(() ->
+                account.deposit(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Amount must be strictly positive");
     }
 }
